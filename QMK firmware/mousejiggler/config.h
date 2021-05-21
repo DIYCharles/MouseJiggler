f79e5fdd80ac391cdc22576463b506b7f4374826
DIYCharles <mcahneriii@gmail.com>
2021-05-21 07:52:44 -0500

    yeet

--- QMK firmware/mousejiggler/config.h
+++ QMK firmware/mousejiggler/config.h
@@ -4,12 +4,12 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x6060
+#define VENDOR_ID       0x0001
+#define PRODUCT_ID      0x0001
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    CharlesTX
-#define PRODUCT         MsJglrkeyboard
-#define DESCRIPTION     MsJglrkeyboard
+#define MANUFACTURER    Microsoft Wired Keyboard
+#define PRODUCT         Microsoft Wired Keyboard
+#define DESCRIPTION     Microsoft Wired Keyboard
 
 /* key matrix size */
 #define MATRIX_ROWS 1
